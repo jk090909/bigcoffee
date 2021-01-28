cddb9b4df7b5286fca98424ed9a042fab106b33a
jk090909 <78129173+jk090909@users.noreply.github.com>
2021-01-28 16:30:37 +0900

    메인 비주얼 이미지 슬라이더 효과

--- js/main.js
+++ js/main.js
@@ -12,3 +12,15 @@ $(function(){
         $('#gnb').hide();
     });
 });
+
+// 이미지 슬라이더 : fade효과
+let num = 1;
+let slider = setInterval(function(){
+  if(num > 2) { num = 0; }
+  // 전체 이미지 fade out
+  $('.slider > img').removeClass('show');
+  // 다음 이미지 표시
+  $('.slider > img').eq(num).addClass('show');
+  console.log(num)
+  num++;
+}, 3000);
